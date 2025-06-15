ce818f932bcb0b836ea4014dbc6476c8ec61dd3c
rahmat-alt <rahmatthidayat0120@gmail.com>
2025-06-15 20:39:31 +0700

    travel

---
+++ index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <section>
+        <div class="container">
+            <div class="navbar">
+                <h1 class="logo" id="home">logo</h1>
+                <ul>
+                    <a href="#home">
+                        <li>home</li>
+                    </a>
+                    <a href="#produk populer">
+                        <li>produk</li>
+                    </a>
+                    <a href="#contact">
+                        <li>contact</li>
+                    </a>
+                </ul>
+                <a href="login.php"><button class="btn_login">login</button></a>
+                <div class="dropdown">
+                    <i class="fa-solid fa-circle-chevron-down"></i>
+                    <div class="dropdown-content">
+                        <a href="#home">Home</a>
+                        <a href="#produk populer">Produk</a>
+                        <a href="#contact">Contact</a>
+                        <a href="login.php">Login</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="text_carousel">
+                <h2>
+                    Lorem ipsum dolor sit amet.
+                </h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, quae. Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <a href="learn_more"><button class="btn_more">learn More</button></a>
+            </div>
+        </div>
+    </section>
+    <!-- content -->
+
+    <div id="produk populer">
+        <h1 class="h1_produk">Produk Populer</h1>
+    </div>
+
+    <nav>
+        <div class="card">
+            <div class="container_card">
+                <div class="card_image">
+                    <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                    <h2>judul 1</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="container_card">
+                <div class="card_image">
+                    <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                    <h2>judul 2</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="container_card">
+                <div class="card_image">
+                    <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                    <h2>judul 3</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="container_card">
+                <div class="card_image">
+                    <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                    <h2>judul 4</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="container_card">
+                <div class="card_image">
+                    <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                    <h2>judul 5</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="container_card">
+                <div class="card_image">
+                    <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                    <h2>judul 6</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                </div>
+            </div>
+        </div>
+        <div class="div">
+            <!-- lainnya -->
+            <h1 class="lainnya">Produk Lainnya</h1>
+            <div class="card_lain">
+                <div class="card">
+                    <div class="container_card">
+                        <div class="card_image">
+                            <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                            <h2>judul </h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="container_card">
+                        <div class="card_image">
+                            <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                            <h2>judul 1</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="container_card">
+                        <div class="card_image">
+                            <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                            <h2>judul 2 </h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="container_card">
+                        <div class="card_image">
+                            <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                            <h2>judul 3 </h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="container_card">
+                        <div class="card_image">
+                            <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                            <h2>judul 4 </h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="container_card">
+                        <div class="card_image">
+                            <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                            <h2>judul 5</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="container_card">
+                        <div class="card_image">
+                            <a href=""><img src="kue_navbar.jpg" alt=""></a>
+                            <h2>judul 6 </h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quibusdam.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </nav>
+    <!-- contact -->
+
+    <contact>
+        <div class="div_alamat">
+            <div class="alamat" id="contact">
+                <ul>
+                    <h1 class="contact_logo">logo</h1>
+                    <a href=""></a>
+                    <li>logo123@gmail.com</li></a>
+                    <li>Jl. tarandam no 1. pdg</li>
+                    <li>085156865566</li>
+                </ul>
+            </div>
+            <div class="contact_nav">
+                <ul>
+                    <a href="">
+                        <li>home</li>
+                    </a>
+                    <a href="">
+                        <li>produk</li>
+                    </a>
+                    <a href="">
+                        <li>contact</li>
+                    </a>
+                </ul>
+            </div>
+            <div class="media_sosial">
+
+                <a href="">
+                    <i class="fa-brands fa-facebook"></i>
+                </a>
+                <a href="">
+                    <i class="fa-brands fa-instagram">
+                    </i>
+                </a>
+                <a href="">
+                    <i class="fa-brands fa-linkedin"></i>
+                </a>
+            </div>
+        </div>
+    </contact>
+    </section>
+
+</body>
+
+</html>
